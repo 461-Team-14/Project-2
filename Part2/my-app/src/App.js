58226dae40328609d7bd4a21145a6024df89ad40
sdlott <sdlott@purdue.edu>
2023-03-03 11:26:56 -0500

    Created an updated react app with login page. Backend still in progress

--- Part2/my-app/src/App.js
+++ Part2/my-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard.js';
+import Login from './components/Login/Login.js';
+import Preferences from './components/Preferences/Preferences.js';
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ECE461 team 14!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/461-Team-14/Project-2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Our Repo
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/preferences' element={<Preferences/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
