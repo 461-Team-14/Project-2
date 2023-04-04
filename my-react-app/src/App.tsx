7ac46bcedfa1875bdca638027744cb0a244447ff
huntingh <huntingh@purdue.edu>
2023-04-03 22:35:08 -0400

    added test script

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,10 +2,8 @@ import React, { useState } from 'react';
 import JSZip from 'jszip';
 
 const App: React.FC = () => {
-  <div>
-    <p>This zip file contains a package.json file.</p>
-  </div>
   const [hasPackageJson, setHasPackageJson] = useState(false);
+
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
--- my-react-app/src/index.tsx
+++ my-react-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TestHaley from './TestHaley'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <TestHaley />
     <App />
   </React.StrictMode>
 );
