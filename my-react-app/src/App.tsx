7f8e647536071a8e8bd35ace8b948c1197d25058
huntingh <huntingh@purdue.edu>
2023-04-03 22:08:57 -0400

    testing

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import JSZip from 'jszip';
 
 const App: React.FC = () => {
+  <div>
+    <p>This zip file contains a package.json file.</p>
+  </div>
   const [hasPackageJson, setHasPackageJson] = useState(false);
-
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
